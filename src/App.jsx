1a6a1fc7451f4debb1f14077e1892bc263275ad1
ManuPullDev <emmanuelfahe9@gmail.com>
2025-11-13 14:26:13 +0000

    api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [infos, setInfos] = useState([])
+
+  useEffect(() => {
+    const GetProducts = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products')
+        const data = await response.json();
+        setInfos(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    GetProducts()
+  },)
 
   const handleClick = () => {
     setCount(count + 1)
@@ -12,27 +24,21 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Salut, Emmanuel comment vas-tu ?</h1>
       <h1>TheMoon BSTEC</h1>
       <div className="card">
         <button onClick={handleClick}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          <ul>
+            {infos.map((products, index) => (
+              <li key={index}>
+                {products.title}
+              </li>
+            ))}
+          </ul>
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
